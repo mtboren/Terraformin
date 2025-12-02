0a244e841278c9393774f4dd12505d76a178204f
Matt Boren <mtboren@users.noreply.github.com>
2025-12-02 22:01:49 +0000

    ➕ Add tf variables, create tf outputs

--- somePlan/main.tf
+++ somePlan/main.tf
@@ -14,27 +14,25 @@ data "aws_ami" "ubuntu_latest" {
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
   most_recent = true
-  owners = ["099720109477"] # 099720109477 is Canonical
+  owners      = ["099720109477"] # 099720109477 is Canonical
 }
 
 ## the VPC subnets tagged as with Type value of "Private"
 data "aws_subnets" "private_subnets" {
   # vpc_id = "vpc-0abc123de456fghij" # replace with your VPC ID
-  tags = {
-    Type = "Private"
-  }
+  tags = { Type = "Private" }
 }
 
 ## get randomized list of private subnet IDs
 resource "random_shuffle" "some_private_subnet_ids" {
-  input        = data.aws_subnets.private_subnets.ids
+  input = data.aws_subnets.private_subnets.ids
   # result_count = 1
 }
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu_latest.id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
   # associate_public_ip_address = false
   subnet_id = random_shuffle.some_private_subnet_ids.result[0]
-  tags          = {otherHotTag = ""}
+  tags      = { otherHotTag = "", Name = var.instance_name }
 }
