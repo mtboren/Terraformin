b653fab68ea931f2e6608eb49b7fb652bff09d7c
Matt Boren <mtboren@users.noreply.github.com>
2025-12-18 21:28:58 +0000

    ➕ Add a VNet and some tags, improve SP operations in snippets

--- Azure/myPlan/main.tf
+++ Azure/myPlan/main.tf
@@ -16,4 +16,18 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "northcentralus"
+
+  tags = {
+    Environment = "tf getting going"
+    Team = "myCoolTeam"
+  }
+  
+}
+
+## some cool virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "northcentralus"
+  resource_group_name = azurerm_resource_group.rg.name
 }
--- Azure/snippetsFromExercise.ps1
+++ Azure/snippetsFromExercise.ps1
@@ -1,6 +1,7 @@
 ## snippets from the exercise
 $strSubscriptionName = "superDev"
 $oThisSubscription = Get-AzSubscription -SubscriptionName $strSubscriptionName
+$strServicePrincipalDisplayName = "terraformAZTesting"
 
 
 #region build, https://developer.hashicorp.com/terraform/tutorials/azure-get-started/azure-build
@@ -8,18 +9,18 @@ $oThisSubscription = Get-AzSubscription -SubscriptionName $strSubscriptionName
 az account set --subscription $strSubscriptionName
 
 ## create a Service Principal
-az ad sp create-for-rbac --display-name terraformAZTesting --role=Contributor --scopes=/subscriptions/$($oThisSubscription.Id) | ConvertFrom-Json -OutVariable oSomeSvcPrincipal
+az ad sp create-for-rbac --display-name $strServicePrincipalDisplayName --role=Contributor --scopes=/subscriptions/$($oThisSubscription.Id) | ConvertFrom-Json -OutVariable oSomeSvcPrincipal
 ## reset Service Principal creds
-az ad sp credential reset --display-name terraformAZTesting | ConvertFrom-Json -OutVariable oSomeSvcPrincipal
+az ad sp credential reset --id (Get-AzADServicePrincipal -DisplayName $strServicePrincipalDisplayName).Id | ConvertFrom-Json -OutVariable oSomeSvcPrincipal
 ## or, reset via PS module cmdlets
-Remove-AzADSpCredential -DisplayName $oSomeSvcPrincipal.displayName
-New-AzADSpCredential -DisplayName $oSomeSvcPrincipal.displayName -EndDate (Get-Date).AddDays(1)
+Remove-AzADSpCredential -DisplayName $strServicePrincipalDisplayName
+New-AzADSpCredential -DisplayName $strServicePrincipalDisplayName -EndDate (Get-Date).AddDays(1)
 
 ## set some env variables for consumption by terraform
-$Env:ARM_CLIENT_ID = ($oSomeSvcPrincipal | ConvertFrom-Json).appId
-$Env:ARM_CLIENT_SECRET = ($oSomeSvcPrincipal | ConvertFrom-Json).password
+$Env:ARM_CLIENT_ID = $oSomeSvcPrincipal.appId
+$Env:ARM_CLIENT_SECRET = $oSomeSvcPrincipal.password
 $Env:ARM_SUBSCRIPTION_ID = $oThisSubscription.Id
-$Env:ARM_TENANT_ID = ($oSomeSvcPrincipal | ConvertFrom-Json).tenant
+$Env:ARM_TENANT_ID = $oSomeSvcPrincipal.tenant
 
 ## do the the usual -- init, validate, plan, apply, show
 terraform init
